e5f8729c2115c9f3b32b6077924ce4f79af08560
salwa a <SalwaBaqer>
2020-11-01 19:55:40 +0300

    cookieshop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import cookies from "./cookies";
+
+const styles = {
+  texts: {
+    textAlign: "center",
+  },
+
+  images: { display: "block", marginLeft: "auto", marginRight: "auto" },
+
+  cookieImage: {
+    marginTop: "100px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    textAlign: "center",
+  },
+};
 
 function App() {
+  const cookiesList = cookies.map((cookie) => (
+    <div>
+      <img src={cookie.image} style={{ height: "50", width: "50" }} />
+      <h2>{cookie.name}</h2>
+      <h3>{cookie.price}</h3>
+    </div>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={styles.texts}>The Best Cookie Shop</h1>
+      <h4 style={styles.texts}>A Delightful Piece of Heaven! </h4>
+      <img
+        src="https://lh3.googleusercontent.com/proxy/K6MGEhq7PLOh7Ev4etvYiR1WAFLCQxH1Zh7XKKLjdLoJ_8RSsdRM90afGoKDuwRXSM6fLX218QqI7pp8C7w_6nnmI4CE9DRi6hhbsSqcqSe8NjOWEbjxH4z8naET8mp3IMrMtDmr0D22qkI-yYFRZpFRIy8xrxc7UuY6"
+        style={styles.images}
+      />
+      <div style={styles.cookieImage}>{cookiesList}</div>
     </div>
   );
 }
