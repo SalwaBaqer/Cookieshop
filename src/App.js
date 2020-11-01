df33b85430381d66de585c9a6412702b2e9a2acc
salwa a <SalwaBaqer>
2020-11-01 20:05:40 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ const styles = {
 
   images: { display: "block", marginLeft: "auto", marginRight: "auto" },
 
-  cookieImage: {
-    marginTop: "100px",
-    marginLeft: "auto",
-    marginRight: "auto",
+  ListWrapper: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    marginTop: "50px",
+    marginLeft: "50px",
+    marginRight: "50px",
     textAlign: "center",
   },
 };
@@ -34,7 +37,7 @@ function App() {
         src="https://lh3.googleusercontent.com/proxy/K6MGEhq7PLOh7Ev4etvYiR1WAFLCQxH1Zh7XKKLjdLoJ_8RSsdRM90afGoKDuwRXSM6fLX218QqI7pp8C7w_6nnmI4CE9DRi6hhbsSqcqSe8NjOWEbjxH4z8naET8mp3IMrMtDmr0D22qkI-yYFRZpFRIy8xrxc7UuY6"
         style={styles.images}
       />
-      <div style={styles.cookieImage}>{cookiesList}</div>
+      <div style={styles.ListWrapper}>{cookiesList}</div>
     </div>
   );
 }
